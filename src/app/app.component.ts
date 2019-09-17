c523ce8623a853e6afc19c30a9623d4a130d228c
Artem Dudar <Artem Dudar@DESKTOP-L5MAP66>
2019-09-17 22:15:51 +0300

    added agGrid package with demo

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,52 @@
-import { Component } from '@angular/core';
+import {Component, OnInit, ViewChild} from '@angular/core';
+import {HttpClient} from '@angular/common/http';
+import {Observable} from 'rxjs';
+import {AgGridAngular} from 'ag-grid-angular';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'agGridTestApp';
+export class AppComponent implements OnInit {
+
+  @ViewChild('agGrid', {static: true}) agGrid: AgGridAngular;
+
+  columnDefs = [
+    {headerName: 'make', field: 'make', sortable: true, filter: true, checkboxSelection: true },
+    {headerName: 'model', field: 'model', sortable: true, filter: true },
+    {headerName: 'price', field: 'price', sortable: true, filter: true }
+  ];
+
+  autoGroupColumnDef = {
+    headerName: 'Model',
+    field: 'model',
+    cellRenderer: 'agGroupCellRenderer',
+    cellRendererParams: {
+      checkbox: true
+    }
+  };
+
+
+  // rowData = [
+  //   { make: 'Toyota', model: 'Celica', price: 35000 },
+  //   { make: 'Ford', model: 'Mondeo', price: 32000 },
+  //   { make: 'Porsche', model: 'Boxter', price: 72000 }
+  // ];
+  rowData: Observable<any>;
+
+  constructor(private http: HttpClient) {
+
+  }
+
+  ngOnInit() {
+    this.rowData = this.http.get('https://api.myjson.com/bins/ly7d1');
+  }
+
+  getSelectedRows() {
+    const selectedNodes = this.agGrid.api.getSelectedNodes();
+    const selectedData = selectedNodes.map( node => node.data );
+    const selectedDataStringPresentation = selectedData.map( node => node.make + ' ' + node.model).join(', ');
+    alert(`Selected nodes: ${selectedDataStringPresentation}`);
+  }
 }
