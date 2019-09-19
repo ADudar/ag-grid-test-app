44c2003eba3000364ad117eff625598bc41e2b6e
Artem Dudar <Artem Dudar@DESKTOP-L5MAP66>
2019-09-20 00:59:48 +0300

    added e2e test scenario

--- e2e/src/app.e2e-spec.ts
+++ e2e/src/app.e2e-spec.ts
@@ -1,23 +1,59 @@
-import { AppPage } from './app.po';
-import { browser, logging } from 'protractor';
+import {GridPage} from './app.po';
 
 describe('workspace-project App', () => {
-  let page: AppPage;
+  let gridPage: GridPage;
 
   beforeEach(() => {
-    page = new AppPage();
+    gridPage = new GridPage();
   });
 
-  it('should display welcome message', () => {
-    page.navigateTo();
-    expect(page.getTitleText()).toEqual('agGridTestApp app is running!');
+  it('should navigate grid page', () => {
+    gridPage.navigateTo();
+    expect(gridPage.gridIsDisplayed()).toBe(true);
   });
 
-  afterEach(async () => {
-    // Assert that there are no errors emitted from the browser
-    const logs = await browser.manage().logs().get(logging.Type.BROWSER);
-    expect(logs).not.toContain(jasmine.objectContaining({
-      level: logging.Level.SEVERE,
-    } as logging.Entry));
+  it('should select all rows', () => {
+    gridPage.waitLoaded();
+    gridPage.clickToggleButton();
+    gridPage.clickHeaderCheckboxButton();
+    gridPage.getCheckedRows().each(row => {
+      expect(row.getAttribute('class')).not.toContain('ag-hidden');
+    });
+    expect(gridPage.getCheckedRows().count()).toBeGreaterThan(1);
   });
+
+  it('get rows from panel', () => {
+    expect(gridPage.getTotalRowsCountFromPanel()).toBeGreaterThan(0);
+  });
+
+  it('should uncheck header checkbox on deselect row', () => {
+    expect(gridPage.getHeaderCheckboxChecked()).toBe(true);
+    gridPage.deSelectRowByIndex(0);
+    expect(gridPage.getHeaderCheckboxChecked()).toBe(false);
+  });
+
+  it('should uncheck all rows by unchecking header checkbox', () => {
+    gridPage.getHeaderCheckboxChecked()
+      .then(checked => {
+        if (checked) {
+          gridPage.clickHeaderCheckboxButton();
+        } else { // deselect all rows by double click
+          gridPage.clickHeaderCheckboxButton();
+          gridPage.clickHeaderCheckboxButton();
+        }
+      })
+      .then(() => {
+        gridPage.getCheckedRows().each(row => {
+          expect(row.getAttribute('class')).toContain('ag-hidden');
+        });
+        expect(gridPage.getHeaderCheckboxChecked()).toBe(false);
+      });
+  });
+
+  it('should hide selection column', () => {
+    gridPage.clickToggleButton();
+    expect(gridPage.getHeaderCheckboxVisible()).toBe(false);
+  });
+
+
 });
--- e2e/src/app.po.ts
+++ e2e/src/app.po.ts
@@ -1,11 +1,66 @@
-import { browser, by, element } from 'protractor';
+import {browser, by, element, protractor} from 'protractor';
 
-export class AppPage {
+export class GridPage {
+  EC = protractor.ExpectedConditions;
+  toggleButton = element(by.buttonText('Toggle selection mode'));
+  headerCheckbox = element(by.css('app-header-checkbox-selection .ag-icon'));
+  loader = element(by.css('.ag-overlay-loading-center'));
+  grid = element(by.css('app-grid'));
+  checkedRows = element.all(by.css('.ag-icon-checkbox-checked'));
+  totalRowsPanel = element(by.css('app-records-count .ag-name-value-value'));
+  selectedRowsPanel = element(by.css('app-selected-records-count .ag-name-value-value'));
+
   navigateTo() {
     return browser.get(browser.baseUrl) as Promise<any>;
   }
 
   getTitleText() {
     return element(by.css('app-root .content span')).getText() as Promise<string>;
+  }
+
+  clickToggleButton() {
+    return this.toggleButton.click();
+  }
+
+  clickHeaderCheckboxButton() {
+    return this.headerCheckbox.click();
+  }
+
+  waitLoaded() {
+    return browser.wait(this.EC.invisibilityOf(this.loader), 15000);
+  }
+
+  gridIsDisplayed() {
+    return this.grid.isDisplayed();
+  }
+
+  getCheckedRows() {
+    return this.checkedRows;
+  }
+
+  getHeaderCheckboxChecked() {
+    return this.headerCheckbox.getAttribute('class')
+      .then((classList => classList.includes('ag-icon-checkbox-checked')));
+  }
+
+  getHeaderCheckboxVisible() {
+    return this.headerCheckbox.isPresent();
+  }
+
+  deSelectRowByIndex(index: number) {
+    const rowSelector = `.ag-center-cols-container [row-index="${index}"]`;
+    const visibleCheckboxSelector = '.ag-icon:not(.ag-hidden)';
+    const visibleCheckbox = element(by.css(rowSelector)).$(visibleCheckboxSelector);
+    visibleCheckbox.getAttribute('class')
+      .then(classList => classList.includes('ag-icon-checkbox-checked'))
+      .then(selected => {
+        if (selected) {
+          visibleCheckbox.click();
+        }
+      });
+  }
+
+  getTotalRowsCountFromPanel() {
+    return this.totalRowsPanel.getText();
   }
 }
